4fd315264858cce112c182df5b9596a274f83d38
Cyberzilla™ <169317950+FJ-cyberzilla@users.noreply.github.com>
2025-09-22 11:58:15 +0330

    Create __init__.py

---
+++ src/detectors/__init__.py
@@ -0,0 +1,4 @@
+# This file declares the 'detectors' directory as a sub-package.
+# It can be used to import key classes or functions directly into the package namespace.
+from .instagram import InstagramDetector
+# from .twitter import TwitterDetector
